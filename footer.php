598e2f18447e8b03d130a3f811ea54bfc9437fe2
OliverGavin <14161044@studentmail.ul.ie>
2016-02-22 22:11:06 +0000

    Initial commit - Project structure

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+    </div><!-- #content -->
+
+    <footer id="main-footer" class="site-footer" role="contentinfo">
+        <div class="site-info">
+
+        </div>
+    </footer>
+</div><!-- #page -->
+
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script type="text/javascript" src="js/jquery-2.2.0.min.js"></script>
+
+</head>
+
+<body>
+<div id="page" class="site">
+
+    <header id="main-header" class="site-header" role="banner">
+        <nav id="main-navigation" class="site-navigation" role="navigation">
+
+        </nav>
+
+        <div class="site-branding">
+            <h1 class="site-title">
+
+            </h1>
+        </div>
+    </header>
+
+    <div id="content" class="site-content">
