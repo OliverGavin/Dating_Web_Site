394c1ce87bc3924db93be4adcd7b26725f45830f
OliverGavin <14161044@studentmail.ul.ie>
2016-03-07 22:05:35 +0000

    Display warnings enabled Added session.php

--- core/init.php
+++ core/init.php
@@ -1,6 +1,10 @@
 <?php
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
 
 require_once 'db/connect.php';
+require_once 'func/session.php';
 require_once 'func/functions.php';
 
 ?>
