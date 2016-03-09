094b4e12f74c87c39524dbdb328cebfff99935ae
chaosHD <14160668@studentmail.ul.ie>
2016-03-09 13:11:07 +0000

    Update login.php

--- login.php
+++ login.php
@@ -11,44 +11,40 @@ require_once 'core/init.php';
     
         <div class="loginOrRegister">
     	
-          <form method="post" action="" onSubmit="">
-  			    <input class="textbox" type="text" size="30" placeholder="Email" name="email"><br>
-  			    <input class="textbox" type="text" size="30" placeholder="Password" name="password">
-  		    </form>
+        	<form method="post" action="" onSubmit="">
+  			<input class="textbox" type="text" size="30" placeholder="Email" name="email"><br>
+  			<input class="textbox" type="text" size="30" placeholder="Password" name="password">
+  		</form>
         
-        <!-- SHOW / HIDE STARTS HERE -->
-          <form method="post" action="" onSubmit="">
-            <div class="checkboxR">
-        	    <input type="checkbox" name="check" id="check" onclick="showHide()"/>
-              <label for="check">Register</label>
-            </div>
+        	<!-- SHOW / HIDE STARTS HERE -->
+          	<form method="post" action="" onSubmit="">
+            		<div class="checkboxR">
+        			<input type="checkbox" name="check" id="check" onclick="showHide()"/>
+              			<label for="check">Register</label>
+            		</div>
             
-            <br> 
+            		<br> 
         
-        	  <div class="hideRegister">
-        
-  				    <input class="textbox" type="text" size="12" placeholder="First Name" name="fname">
-  				    <input class="textbox" type="text" size="12" placeholder="Last Name" name="lName">
-                
-              <br>
-                
-                <div class="radioGender">
+        	  	<div class="hideRegister">
+        			<input class="textbox" type="text" size="12" placeholder="First Name" name="fname">
+  				<input class="textbox" type="text" size="12" placeholder="Last Name" name="lName">
+                		<br>
+                		<div class="radioGender">
 					<input id="male" type="radio" name="gender" value="male">
 					<label for="male">Male</label>
 					<input id="female" type="radio" name="gender" value="female">
 					<label for="female">Female</label>
 				</div>
-                
-                <br>
-                <div class="selectLogin">
-                 <select id="day" name="day">
+                		<br>
+                		<div class="selectLogin">
+                 			<select id="day" name="day">
 					       `<option value="-">Day</option>
 					        <option value="1">1</option>
 					        <option value="2">2</option>
 					        <option value="3">3</option>
 					        <option value="4">4</option>
 				        </select>
-                <select id="month" name="month">
+                			<select id="month" name="month">
 					        <option value="-">Month</option>
 					        <option value="1">January</option>
 					        <option value="2">Febuary</option>
@@ -63,11 +59,26 @@ require_once 'core/init.php';
 					        <option value="2007">2007</option>
 					        <option value="2006">2006</option>
 				        </select>
-              </div>
-                
-        	</div>
-        </form>
-	    </div>
+              			</div>
+                	</div>
+        	</form>
+	</div>
+<!-- Just put this here for now your more than welcome to move it -->
+	<script type="text/javascript">
+		function showHide(){
+			var checkbox = document.getElementById("check");
+			var hiddeninputs = document.getElementsByClassName("hideRegister");
+			
+			for(var i = 0; i != hiddeninputs.length; i++){
+				if(checkbox.checked){
+						hiddeninputs[i].style.display = "block";
+				}
+				else{
+					hiddeninputs[i].style.display = "none";
+				}
+			}
+		}
+    	</script>
         
     </main><!-- #main -->
 </div><!-- #primary -->
