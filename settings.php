b4b53c198f52e20b07f3ef23b0155271a675d6e7
chaosHD <14160668@studentmail.ul.ie>
2016-03-12 11:01:38 +0000

    Update settings.php

--- settings.php
+++ settings.php
@@ -7,27 +7,25 @@ require_once 'core/init.php';
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
         
-            <!-- CONTENT STARTS HERE -->
+        <!-- CONTENT STARTS HERE -->
     
-    <div class="generalSettings">
+    	<div class="generalSettings">
     
-    	<h2>General Account Settings</h2>
+    		<h2>General Account Settings</h2>
         
-        <form>
+        	<form>
 			<label for="firstname">Name:</label>        <input class="textbox" type="text" name="firstname" size="30" placeholder="Jane"/><br>
 			<label for="surname">Surname:</label>   <input class="textbox" type="text" name="surname" size="30" placeholder="Doe"/><br>
 			<label for="email">Email: </label>      <input class="textbox" type="text" name="email" size="30" placeholder="janedoe@gmail.com"/><br>
-            <label for="email">Password: </label>      <input class="textbox" type="text" name="oldPassword" size="30" placeholder="Old Password"/><br>
-            <label for="email"> </label>      <input class="textbox" type="text" name="newPassword" size="30" placeholder="New Password"/><br>
+            		<label for="email">Password: </label>      <input class="textbox" type="text" name="oldPassword" size="30" placeholder="Old Password"/><br>
+            		<label for="email"> </label>      <input class="textbox" type="text" name="newPassword" size="30" placeholder="New Password"/><br>
 			<br class="clear" />
-            <input class="button" type="submit" value="Save Changes" />
-            <br class="clear"/>
+            		<input class="button" type="submit" value="Save Changes" />
+            		<br class="clear"/>
 		</form>
-
+	</div>
     
-    </div>
-    
-    <!--CONTENT HERE -->
+    	<!--CONTENT ENDS HERE -->
         
     </main><!-- #main -->
 </div><!-- #primary -->
