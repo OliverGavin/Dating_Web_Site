036dc442c9c14867005243af8017ceb78dca9a3f
OliverGavin <14161044@studentmail.ul.ie>
2016-03-16 00:11:06 +0000

    Added default timezone to correct server time errors Added array to store error/success messages

--- core/init.php
+++ core/init.php
@@ -3,6 +3,13 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 
+date_default_timezone_set('GMT');
+
+// reinitialise error/success messages
+$message = array();
+$message['error'] = array();
+$message['success'] = array();
+
 require_once 'db/connect.php';
 require_once 'func/session.php';
 require_once 'func/functions.php';
