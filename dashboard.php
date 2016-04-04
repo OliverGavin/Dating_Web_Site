5c879750f601fe4435c3d93f54012e05b13648a6
chaosHD <14160668@studentmail.ul.ie>
2016-04-04 12:36:52 +0100

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,114 @@
+<?php
+require_once 'core/init.php';
+?>
+
+<?php get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+    <!-- CONTENT STARTS HERE -->
+    
+    <div id="featured">
+    	<div class="featuredHead">
+        	<p><b>Featured Users</b></p>
+        </div>
+    	<div class="pictures">
+            
+            <ul>
+          		<li><span><a href="#"><img src="profile.png"></a></span>
+                <span>Jane Doe</span>
+                </li>
+          		<li><span><a href="#"><img src="profile.png"></a></span>
+                <span>Jane Doe</span>
+                </li>
+          		<li><span><a href="#"><img src="profile.png"></a></span>
+                <span>Jane Doe</span>
+                </li>
+          		<li><span><a href="#"><img src="profile.png"></a></span>
+                <span>Jane Doe</span>
+                </li>
+                <li><span><a href="#"><img src="profile.png"></a></span>
+                <span>Jane Doe</span>
+                </li>
+                <li><span><a href="#"><img src="profile.png"></a></span>
+                <span>Jane Doe</span>
+                </li>
+                <li><span><a href="#"><img src="profile.png"></a></span>
+                <span>Jane Doe</span>
+                </li>
+                <li><span><a href="#"><img src="profile.png"></a></span>
+                <span>Jane Doe</span>
+                </li>
+			</ul>
+            
+        </div>
+    </div>
+    
+    <div id="notifications">
+    	<div class="notificationHead">
+        	<p><b>Notifications</b></p>
+        </div>
+    	<div class="notePictures">
+            
+            <ul>
+          		<li><span><img src="profile.png"></span>
+                <span>Hi, i'm jane wanna go out tonight?</span>
+                <span><i class="fa fa-trash" onClick=""></i></span>
+                </li>
+                
+                <li><span><img src="profile.png"></span>
+                <span>Jane Doe has liked your profile, like back to chat.</span>
+                <span><i class="fa fa-trash" onClick=""></i></span>
+                </li>
+                
+                <li><span><img src="profile.png"></span>
+                <span>Hello</span>
+                <span><i class="fa fa-trash" onClick=""></i></span>
+                </li>
+                
+                <li><span><img src="profile.png"></span>
+                <span>Jane Doe has disliked your profile, see are tips to get more likes!</span>
+                <span><i class="fa fa-trash" onClick=""></i></span>
+                </li>
+                
+                <li><span><img src="profile.png"></span>
+                <span>Jane Doe has liked your profile, like back to chat.</span>
+                <span><i class="fa fa-trash" onClick=""></i></span>
+                </li>
+                
+                <li><span><img src="profile.png"></span>
+                <span>You have been giving a warning by an admin, be careful!</span>
+                <span><i class="fa fa-trash" onClick=""></i></span>
+                </li>
+			</ul>
+            
+        </div>
+    </div>
+    
+    <div id="pOverview">
+    	<div class="pOverviewLink">
+        	<i class="fa fa-user fa-2x"></i>
+            <p><b>Your Profile</b></p>
+        </div>
+    
+    	<img src="profile.png">
+        <p>Jane Doe</p>
+        <p>22, Single</p>
+            
+    </div>
+    
+    <div id="recentlyViewedYou">
+    	<div class="recentlyViewedYouHead">
+        	<p><b>Recently Viewed You</b></p>
+        </div>
+    
+    </div>
+    <div id="clearDash"></div>
+    
+    <!--CONTENT HERE -->
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
