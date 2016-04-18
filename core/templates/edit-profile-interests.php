1973ce94b0b463c7cda9dfc672f569c8f5dc3782
OliverGavin <14161044@studentmail.ul.ie>
2016-04-19 00:00:52 +0100

    Added more consistent titles Moved delete to the left hand side

--- core/templates/edit-profile-interests.php
+++ core/templates/edit-profile-interests.php
@@ -3,22 +3,22 @@
 $likes = get_interests($user_id, true);
 
 echo '<div class="profile-field profile-interests profile-likes">';
-    echo '<h3>Likes</h3>';
+    echo '<label for="new_interest_like">Likes</label>';
 
-    echo '<ul>';
+    echo '<ul style="list-style: none">';
     if (isset($likes) && !empty($likes)) {
 
         foreach ($likes as $like) {
             echo '<li class="interest interest-like">';
-                echo '<span class="interest-content">';
-                    echo $like->content;
-                echo '</span>';
-
                 // link is a rollback if JS is not present
                 $link = $_SERVER['REQUEST_URI'] . (isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING'])? '&':'?') . 'delete_interest=' . $like->interests_id;
                 echo '<a href="' . $link . '" class="interest-action interest-delete" onclick="remove_interest(this,'.$like->interests_id.')" title="delete">';
                     echo '<i class="fa fa-times"></i>';
                 echo '</a>';
+                echo '<span class="interest-content">';
+                    echo $like->content;
+                echo '</span>';
+
             echo '</li>';
         }
     }
@@ -33,22 +33,22 @@ echo '</div>';
 $dislikes = get_interests($user_id, false);
 
 echo '<div class="profile-field profile-dislikes">';
-    echo '<h3>Dislikes</h3>';
+    echo '<label for="new_interest_dislike">Dislikes</label>';
 
-    echo '<ul>';
+    echo '<ul style="list-style: none">';
     if (isset($dislikes) && !empty($dislikes)) {
 
             foreach ($dislikes as $dislike) {
                 echo '<li class="interest interest-like">';
-                    echo '<span class="interest-content">';
-                        echo $dislike->content;
-                    echo '</span>';
-
                     // link is a rollback if JS is not present
                     $link = $_SERVER['REQUEST_URI'] . (isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING'])? '&':'?') . 'delete_interest=' . $dislike->interests_id;
                     echo '<a href="' . $link . '" class="interest-action interest-delete" onclick="remove_interest(this,'.$dislike->interests_id.')" title="delete">';
                         echo '<i class="fa fa-times"></i>';
                     echo '</a>';
+                    echo '<span class="interest-content">';
+                        echo $dislike->content;
+                    echo '</span>';
+
                 echo '</li>';
             }
         }
