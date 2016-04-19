b23cc3c42b525c750ebe14b1902acc986b06c45b
OliverGavin <14161044@studentmail.ul.ie>
2016-04-19 01:12:00 +0100

    Validation, only displays errors on the fields being used

--- settings.php
+++ settings.php
@@ -1,31 +1,70 @@
 <?php
 require_once 'core/init.php';
+require_once 'core/func/validation.php';
 
 verify_login();
+
+$valid_password = false;
 
 $first_name = $_SESSION['first_name'];
 $last_name = $_SESSION['last_name'];
 
+$changing_email = false;
+$changing_password = false;
+
 if (isset($_POST['action']) && $_POST['action'] === 'Save Changes') {
 	if (!isset($_POST['current_password']) || !check_password($_POST['current_password'])) {
-		echo 'invalid password';
-	} else {
-		// TODO validation
-		if (isset($_POST['email1']) && isset($_POST['email2']) && !empty($_POST['email1']) && !empty($_POST['email2'])) {
-			if ($_POST['email1'] != $_POST['email2']) {
-				echo 'emails must be the same';
-			} else {
-				set_email($_POST['email1']);
-			}
-		}
+		$_SESSION['form_errors']['current_password'] = 'incorrect password.';
+	} else $valid_password = true;
 
-		if (isset($_POST['new_password1']) && isset($_POST['new_password2']) && !empty($_POST['new_password1']) && !empty($_POST['new_password2'])) {
-			if ($_POST['new_password1'] != $_POST['new_password2']) {
-				echo 'passwords must be the same';
-			} else {
-				set_password($_POST['new_password1']);
+	if (isset($_POST['email1']) && isset($_POST['email2'])){
+		$changing_email = true;
+		if (!empty($_POST['email1'])) {
+			$email1 = validate_email($_POST['email1'], 'email1');
+
+			if (!empty($_POST['email2'])) {
+				$email2 = $_POST['email2'];
+				if ($email1 != $email2) {
+					$_SESSION['form_errors']['email2'] = 'emails must be the same.';
+				}
 			}
 		}
+		if (!empty($_POST['email1']) && !empty($_POST['email2'])) {
+			if ($valid_password && empty($_SESSION['form_errors'])) {
+				set_email($email1);
+			}
+		} else if (!empty($_POST['email1']) && empty($_POST['email2'])) {
+			$_SESSION['form_errors']['email2'] = 'Both email fields required.';
+
+		} else if (empty($_POST['email1']) && !empty($_POST['email2'])) {
+			$_SESSION['form_errors']['email1'] = 'Both email fields required.';
+
+		} else $changing_email = false;
+	}
+
+	if (isset($_POST['new_password1']) && isset($_POST['new_password2'])) {
+		$changing_password = true;
+		if (!empty($_POST['new_password1'])) {
+			$new_password1 = validate_password($_POST['new_password1'], 'new_password1');
+
+			if (!empty($_POST['new_password2'])) {
+				$new_password2 = $_POST['new_password2'];
+				if ($new_password1 != $new_password2) {
+					$_SESSION['form_errors']['new_password2'] = 'passwords must be the same.';
+				}
+			}
+		}
+		if (!empty($_POST['new_password1']) && !empty($_POST['new_password2'])) {
+			if ($valid_password && empty($_SESSION['form_errors'])) {
+				set_password($new_password1);
+			}
+		} else if (!empty($_POST['new_password1']) && empty($_POST['new_password2'])) {
+			$_SESSION['form_errors']['new_password2'] = 'Both password fields required.';
+
+		} else if (empty($_POST['new_password1']) && !empty($_POST['new_password2'])) {
+			$_SESSION['form_errors']['new_password1'] = 'Both password fields required.';
+
+		} else $changing_password = false;
 	}
 }
 
@@ -102,38 +141,44 @@ function set_email($email) {
 
 				<br><br>
 
-				<div class="group both-rounded">
+				<div class="group both-rounded <?php if($changing_email) echo get_form_field_status('email1'); ?>">
 					<label for="email" class="visible">New email</label>
-					<input class="textbox" type="email" name="email1" size="30" placeholder="janedoe@gmail.com"/>
+					<input class="textbox" type="email" name="email1" size="30" placeholder="janedoe@gmail.com" value="<?php if(isset($email1)) echo $email1?>"/>
 				</div>
+				<?php if($changing_email) echo get_form_field_message('email1');  ?>
 
-				<div class="group both-rounded">
+				<div class="group both-rounded <?php if($changing_email && !empty($email1)) echo get_form_field_status('email2'); ?>">
 					<label for="email" class="visible">Confirm email</label>
-					<input class="textbox" type="email" name="email2" size="30" placeholder="janedoe@gmail.com"/>
+					<input class="textbox" type="email" name="email2" size="30" placeholder="janedoe@gmail.com" value="<?php if(isset($email2)) echo $email2?>"/>
 				</div>
+				<?php if($changing_email && !empty($email1)) echo get_form_field_message('email2');  ?>
 
 				<br><br>
 
-				<div class="group both-rounded">
+				<div class="group both-rounded <?php if($changing_password) echo get_form_field_status('new_password1'); ?>">
 					<label for="email" class="visible">New Password</label>
-					<input class="textbox" type="password" name="new_password1" size="30" placeholder=""/>
+					<input class="textbox" type="password" name="new_password1" size="30" value="<?php if(isset($new_password1)) echo $new_password1?>"/>
 				</div>
+				<?php if($changing_password) echo get_form_field_message('new_password1');  ?>
 
-				<div class="group both-rounded">
+				<div class="group both-rounded <?php if($changing_password && !empty($new_password1)) echo get_form_field_status('new_password2'); ?>">
 					<label for="email" class="visible">Confirm Password</label>
-					<input class="textbox" type="password" name="new_password2" size="30" placeholder=""/>
+					<input class="textbox" type="password" name="new_password2" size="30" value="<?php if(isset($new_password2)) echo $new_password2?>"/>
 				</div>
+				<?php if($changing_password && !empty($new_password1)) echo get_form_field_message('new_password2');  ?>
 
 				<br><br><br>
 
-				<div class="group both-rounded">
+				<div class="group both-rounded <?= get_form_field_status('current_password'); ?>">
 					<label for="email" class="visible">Current password</label>
 					<input class="textbox" type="password" name="current_password" size="30" placeholder=""/>
 				</div>
+				<?= get_form_field_message('current_password');  ?>
 
 				<br>
 
 				<input class="button" type="submit" name="action" value="Save Changes" />
+				<br>
 				<br>
 				<br>
 				<a class="button" href="search.php?action=browse&blocked=true">Manage blocked users</a>
